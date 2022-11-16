74900d5703bc8b97139270b488064d84d50d80b1
Flavien Bonvin <flavien@coteries.com>
2022-11-16 20:31:25 +0100

    Add custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
 import "./globals.css"
 
+import { IBM_Plex_Sans } from "@next/font/google"
+
+const font = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["400", "500", "600", "700"],
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={font.variable}>
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="flex h-10 items-center justify-center px-10 font-bold shadow-sm">
+          Fancy calendars
+        </div>
+        <main className="container mx-auto pt-4">{children}</main>
+      </body>
     </html>
   )
 }
